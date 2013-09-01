e79c4e7cb8ede1955eab6e214b61f89235aeab03
Constantin Baranov <const@mimas.ru>
2013-09-01 16:12:05 +0300

    Add missing copyright stuff

--- avformat.c
+++ avformat.c
@@ -1,3 +1,23 @@
+/* GLGrab AVInputFormat
+ *
+ * Copyright 2013 Constantin Baranov
+ *
+ * This file is part of GLGrab.
+ *
+ * GLGrab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GLGrab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GLGrab.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #define _XOPEN_SOURCE 700
 
 #include "glgrab.h"
