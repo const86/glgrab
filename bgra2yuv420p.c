82c6494f0c3230eed8ebf482999025cc9f3f5651
Constantin Baranov <const@mimas.ru>
2013-10-05 03:33:20 +0300

    Use 32x2 blocks, and refactor code a bit

--- bgra2yuv420p.c
+++ bgra2yuv420p.c
@@ -19,6 +19,7 @@
  */
 
 #include "bgra2yuv420p.h"
+#include <string.h>
 
 #define BT_709_KB 0.0722
 #define BT_709_KR 0.2126
@@ -88,7 +89,20 @@ static void bgra2yuv420p_2x2(const void *restrict bgra0, const void *restrict bg
 	*(uint8_t *)v = _mm_extract_pi16(yyyyu0v0, 3);
 }
 
-static void bgra2yuv420p_16x2(const void *restrict bgra0, const void *restrict bgra1,
+static __m128i avg(const __m128i p0[2], const __m128i p1[2]) {
+	const __m128 a0 = _mm_castsi128_ps(_mm_avg_epu8(p0[0], p1[0]));
+	const __m128 a1 = _mm_castsi128_ps(_mm_avg_epu8(p0[1], p1[1]));
+	return _mm_avg_epu8(_mm_castps_si128(_mm_shuffle_ps(a0, a1, _MM_SHUFFLE(2, 0, 2, 0))),
+		_mm_castps_si128(_mm_shuffle_ps(a0, a1, _MM_SHUFFLE(3, 1, 3, 1))));
+}
+
+static void dot(void *c, const __m128i p[4], __m128i K, __m128i B, int S) {
+	const __m128i c0 = _mm_add_epi16(B, _mm_hadd_epi16(_mm_maddubs_epi16(p[0], K), _mm_maddubs_epi16(p[1], K)));
+	const __m128i c2 = _mm_add_epi16(B, _mm_hadd_epi16(_mm_maddubs_epi16(p[2], K), _mm_maddubs_epi16(p[3], K)));
+	_mm_storeu_si128(c, _mm_packus_epi16(_mm_srli_epi16(c0, S), _mm_srli_epi16(c2, S)));
+}
+
+static void bgra2yuv420p_32x2(const void *restrict bgra0, const void *restrict bgra1,
 	void *restrict y0, void *restrict y1, void *restrict u, void *restrict v) {
 	const __m128i Y = _mm_set_epi8(0, KRYi, KGYi, KBYi, 0, KRYi, KGYi, KBYi,
 		0, KRYi, KGYi, KBYi, 0, KRYi, KGYi, KBYi);
@@ -100,60 +114,40 @@ static void bgra2yuv420p_16x2(const void *restrict bgra0, const void *restrict b
 		0, KRVi, KGVi, KBVi, 0, KRVi, KGVi, KBVi);
 	const __m128i Cb = _mm_set1_epi16(Cbias);
 
-	const __m128i *p0 = bgra0;
-	const __m128i *p1 = bgra1;
+	__m128i p0[8], p1[8];
+	memcpy(p0, bgra0, sizeof(p0));
+	memcpy(p1, bgra1, sizeof(p1));
 
-	const __m128i p00 = _mm_loadu_si128(p0 + 0);
-	const __m128i p01 = _mm_loadu_si128(p0 + 1);
-	const __m128i p02 = _mm_loadu_si128(p0 + 2);
-	const __m128i p03 = _mm_loadu_si128(p0 + 3);
-	const __m128i p10 = _mm_loadu_si128(p1 + 0);
-	const __m128i p11 = _mm_loadu_si128(p1 + 1);
-	const __m128i p12 = _mm_loadu_si128(p1 + 2);
-	const __m128i p13 = _mm_loadu_si128(p1 + 3);
+	dot((__m128i *)y0 + 0, p0 + 0, Y, Yb, SY);
+	dot((__m128i *)y0 + 1, p0 + 4, Y, Yb, SY);
+	dot((__m128i *)y1 + 0, p1 + 0, Y, Yb, SY);
+	dot((__m128i *)y1 + 1, p1 + 4, Y, Yb, SY);
 
-	const __m128 a0 = _mm_castsi128_ps(_mm_avg_epu8(p00, p10));
-	const __m128 a1 = _mm_castsi128_ps(_mm_avg_epu8(p01, p11));
-	const __m128 a2 = _mm_castsi128_ps(_mm_avg_epu8(p02, p12));
-	const __m128 a3 = _mm_castsi128_ps(_mm_avg_epu8(p03, p13));
+	const __m128i a[4] = {
+		avg(p0 + 0, p1 + 0), avg(p0 + 2, p1 + 2),
+		avg(p0 + 4, p1 + 4), avg(p0 + 6, p1 + 6)
+	};
 
-	const __m128i a01 = _mm_avg_epu8(_mm_castps_si128(_mm_shuffle_ps(a0, a1, _MM_SHUFFLE(2, 0, 2, 0))),
-		_mm_castps_si128(_mm_shuffle_ps(a0, a1, _MM_SHUFFLE(3, 1, 3, 1))));
-	const __m128i a23 = _mm_avg_epu8(_mm_castps_si128(_mm_shuffle_ps(a2, a3, _MM_SHUFFLE(2, 0, 2, 0))),
-		_mm_castps_si128(_mm_shuffle_ps(a2, a3, _MM_SHUFFLE(3, 1, 3, 1))));
-
-	const __m128i y00 = _mm_add_epi16(Yb, _mm_hadd_epi16(_mm_maddubs_epi16(p00, Y), _mm_maddubs_epi16(p01, Y)));
-	const __m128i y02 = _mm_add_epi16(Yb, _mm_hadd_epi16(_mm_maddubs_epi16(p02, Y), _mm_maddubs_epi16(p03, Y)));
-	_mm_storeu_si128(y0, _mm_packus_epi16(_mm_srli_epi16(y00, SY), _mm_srli_epi16(y02, SY)));
-
-	const __m128i y10 = _mm_add_epi16(Yb, _mm_hadd_epi16(_mm_maddubs_epi16(p10, Y), _mm_maddubs_epi16(p11, Y)));
-	const __m128i y12 = _mm_add_epi16(Yb, _mm_hadd_epi16(_mm_maddubs_epi16(p12, Y), _mm_maddubs_epi16(p13, Y)));
-	_mm_storeu_si128(y1, _mm_packus_epi16(_mm_srli_epi16(y10, SY), _mm_srli_epi16(y12, SY)));
-
-	const __m128i u0 = _mm_add_epi16(Cb, _mm_hadd_epi16(_mm_maddubs_epi16(a01, U), _mm_maddubs_epi16(a23, U)));
-	const __m128i v0 = _mm_add_epi16(Cb, _mm_hadd_epi16(_mm_maddubs_epi16(a01, V), _mm_maddubs_epi16(a23, V)));
-	const __m128 uv = _mm_castsi128_ps(_mm_packus_epi16(_mm_srli_epi16(u0, SC), _mm_srli_epi16(v0, SC)));
-
-	_mm_storel_pi(u, uv);
-	_mm_storeh_pi(v, uv);
+	dot(u, a, U, Cb, SC);
+	dot(v, a, V, Cb, SC);
 }
 
 void bgra2yuv420p(const uint8_t *restrict bgra, ptrdiff_t bgra_stride,
 	uint8_t *restrict y, ptrdiff_t y_stride, uint8_t *restrict u, ptrdiff_t u_stride,
 	uint8_t *restrict v, ptrdiff_t v_stride, size_t width, size_t height) {
-	const size_t step16 = width / 16;
-	const size_t step2 = width % 16 / 2;
+	const size_t step32 = width / 32;
+	const size_t step2 = width % 32 / 2;
 
 	for (size_t row2 = 0; row2 < height / 2; row2++) {
 		const uint8_t *p0 = bgra;
 		uint8_t *y0 = y, *u0 = u, *v0 = v;
 
-		for (size_t i = 0; i < step16; i++) {
-			bgra2yuv420p_16x2(p0, p0 + bgra_stride, y0, y0 + y_stride, u0, v0);
-			p0 += 16 * 4;
-			y0 += 16;
-			u0 += 8;
-			v0 += 8;
+		for (size_t i = 0; i < step32; i++) {
+			bgra2yuv420p_32x2(p0, p0 + bgra_stride, y0, y0 + y_stride, u0, v0);
+			p0 += 32 * 4;
+			y0 += 32;
+			u0 += 16;
+			v0 += 16;
 		}
 
 		for (size_t i = 0; i < step2; i++) {
